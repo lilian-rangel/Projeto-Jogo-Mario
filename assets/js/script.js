a5f9886165c2e88169b2a9266380ab94edd9ec57
Giulliano Guimarães <llthguimaraes@gmail.com>
2023-01-02 14:35:37 -0300

    jump animation and theme sound

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,56 @@
 const mario = document.querySelector(".mario");
 const pipe = document.querySelector(".pipe");
 const clouds = document.querySelector(".clouds");
-const audio = document.querySelector(".audio");
+const actions = document.querySelector(".actions");
+const actionsBtn = actions.querySelector(".actions_btn");
 
-const themeSfx = () => new Audio("assets/sfx/super_mario_world_overworld_theme.mp3");
-const jumpSfx = () => new Audio("assets/sfx/super_mario_world_jump_sound_effect.mp3");
-const deathSfx = () => new Audio("assets/sfx/super_mario_world_death.mp3");
+const themeSfx = new Audio("assets/sfx/super_mario_world_overworld_theme.mp3");
+const jumpSfx = new Audio("assets/sfx/super_mario_world_jump_sound_effect.mp3");
+const deathSfx = new Audio("assets/sfx/super_mario_world_death.mp3");
+
+const pipePosition = () => pipe.offsetLeft;
+const cloudsPosition = () => clouds.offsetLeft;
+const marioPosition = () => +window.getComputedStyle(mario).bottom.replace("px", "");
+
+const deathPosition = () => pipePosition() <= 120 && pipePosition() > 0 && marioPosition() < 80;
 
 const jump = () => {
-    mario.classList.add("jump");
-    mario.src = "./assets/img/jump.png";
+
+    !deathPosition() && mario.classList.add("jump");
+    !deathPosition() && jumpSfx.play();
+
+    mario.src = deathPosition() ? "./assets/img/game-over.png" : "./assets/img/jump.png";
 
     setTimeout(() => {
         mario.classList.remove("jump");
-        mario.src = "./assets/img/mario.gif";
+        mario.src = deathPosition() ? "./assets/img/game-over.png" : "./assets/img/mario.gif";
     }, 500);
-
-    jumpSfx().play();
 }
 
 const loop = setInterval(() => {
 
-    const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
-    const cloudsPosition = clouds.offsetLeft;
+    if (deathPosition()) {
 
-    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+        let marioPos = marioPosition();
+        let pipePos = pipePosition();
+        let cloudsPos = cloudsPosition();
 
-        pipe.style.animation = "none";
-        pipe.style.left = `${pipePosition}px`;
+        pipe.classList.remove("play");
+        clouds.classList.remove("play");
 
-        mario.style.animation = "none";
-        mario.style.bottom = `${marioPosition}px`;
+        pipe.style.left = `${pipePos}px`;
+
+        mario.style.animation = "";
+        mario.style.bottom = `${marioPos}px`;
 
         mario.src = "./assets/img/game-over.png";
         mario.style.width = "70px";
         mario.style.marginLeft = "50px";
 
-        clouds.style.animation = "none";
-        clouds.style.left = `${cloudsPosition}px`;
+        clouds.style.left = `${cloudsPos}px`;
 
-        deathSfx().play();
+        themeSfx.pause();
+        deathSfx.play();
 
         clearInterval(loop);
 
@@ -48,5 +58,15 @@ const loop = setInterval(() => {
 
 }, 10);
 
-document.addEventListener("keydown", jump);
-playAudio();
+const startGame = () => {
+    actions.style.display = "none";
+    pipe.classList.add("play");
+    clouds.classList.add("play");
+    themeSfx.play();
+    themeSfx.volume = 0.6;
+    document.removeEventListener("keydown", startGame);
+    document.addEventListener("keydown", jump);
+}
+
+actionsBtn.addEventListener("click", startGame);
+document.addEventListener("keydown", startGame);
